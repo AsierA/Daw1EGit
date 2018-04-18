d98acc858d14f0f953fd18add79ea7a45d655856
AsierA <IEUser@192.168.190.134>
2018-04-18 08:40:05 -0700

    11

--- HelloEGit/src/helloEGit.java
+++ HelloEGit/src/helloEGit.java
@@ -2,14 +2,12 @@
 public class helloEGit {
 	public static void main(String[] args) {
 		//Comentario de prueba
-<<<<<<< HEAD
 		//Segundo comentario
 		System.out.println("Hola EGit 2");
 		System.out.println("Por fin");
 		System.out.println("Cambio de prueba");
-=======
+
 		hola hola = new hola();
 		hola.saludo();
->>>>>>> refs/remotes/Daw1EGit/master
 	}
 }
