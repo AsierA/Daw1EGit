adaa182b655efe51d2aa92f094555d61fb118f2b
AsierA <IEUser@192.168.190.134>
2018-04-18 08:21:23 -0700

    Segundo comentario

--- HelloEGit/src/helloEGit.java
+++ HelloEGit/src/helloEGit.java
@@ -2,6 +2,7 @@
 public class helloEGit {
 	public static void main(String[] args) {
 		//Comentario de prueba
+		//Segundo comentario
 		System.out.println("Hola EGit 2");
 		System.out.println("Por fin");
 		System.out.println("Cambio de prueba");
