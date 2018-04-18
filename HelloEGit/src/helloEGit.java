f501a30ba11d667874e1104a30ccf216fdcfb55e
AsierA <IEUser@192.168.190.134>
2018-04-18 08:41:08 -0700

    12

--- HelloEGit/src/helloEGit.java
+++ HelloEGit/src/helloEGit.java
@@ -3,10 +3,6 @@ public class helloEGit {
 	public static void main(String[] args) {
 		//Comentario de prueba
 		//Segundo comentario
-		System.out.println("Hola EGit 2");
-		System.out.println("Por fin");
-		System.out.println("Cambio de prueba");
-
 		hola hola = new hola();
 		hola.saludo();
 	}
