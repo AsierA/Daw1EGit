69f8485fb537b3c07be61363a400f4140125abb3
AsierA <IEUser@192.168.190.134>
2018-04-18 08:27:48 -0700

    Creacion de nueva clase y modificacion del main

--- HelloEGit/src/helloEGit.java
+++ HelloEGit/src/helloEGit.java
@@ -2,8 +2,7 @@
 public class helloEGit {
 	public static void main(String[] args) {
 		//Comentario de prueba
-		System.out.println("Hola EGit 2");
-		System.out.println("Por fin");
-		System.out.println("Cambio de prueba");
+		hola hola = new hola();
+		hola.saludo();
 	}
 }
---
+++ HelloEGit/src/hola.java
@@ -0,0 +1,11 @@
+
+public class hola {
+	
+	public hola(){
+		
+	}
+	
+	public void saludo(){
+		System.out.println("Hola EGit");
+	}
+}
